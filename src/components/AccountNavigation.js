f61fd8ea73f1dddffadcb4d078ebca1a5ab7c1e4
Kyle Johnson <kbjohnson90+github@gmail.com>
2020-12-18 20:27:32 -0500

    chore: Center avatar image.

--- src/components/AccountNavigation.js
+++ src/components/AccountNavigation.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 export default function() {
     return (
         <Link to="/account">
-            <img className="inline-block m-auto mt-0 mb-0 h-12 w-12 rounded-full" src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""></img>
+            <img className="block m-auto mt-0 mb-0 h-12 w-12 rounded-full" src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""></img>
         </Link>
     )
 }
