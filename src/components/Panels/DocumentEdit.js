a93a8804adafd98a2e6ad7d8ae3ca43c914f27fc
Kyle Johnson <kbjohnson90+github@gmail.com>
2021-01-16 19:12:41 -0500

    feature: Add header block.

--- src/components/Panels/DocumentEdit.js
+++ src/components/Panels/DocumentEdit.js
@@ -1,11 +1,17 @@
 import React from "react";
 import EditorJs from 'react-editor-js'
+import Header from "@editorjs/header"
 
 export default function Document() {
+
+    const tools = {
+        header: Header,
+    }
+
     return (
         <>
             <div className="w-full h-96 p-8 border border-gray-400 rounded">
-                <EditorJs data={{
+                <EditorJs tools={tools} data={{
                     blocks: [
                         {
                             type: "paragraph",
